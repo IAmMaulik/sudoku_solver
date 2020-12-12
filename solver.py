5e89d60cb7187c489df1e903295b90479e9ea702
Maulik Shah <maulik.shah715@gmail.com>
2020-12-12 14:07:44 +0530

    Added a funcion that finds empty squares on the board

--- solver.py
+++ solver.py
@@ -12,8 +12,16 @@ def print_board(bo):
             else:
                 print(str(bo[i][j]) + " ", end="")
 
+# Finding empty squares on the sudoku board
+def find_empty(bo):
+    for x in range(len(bo)):
+        for y in range(len(bo[0])):
+            if bo[x][y] == 0:
+                return (x, y) # Coordinates of the empty square
+
 # Defing the 2d array
-board = [[7, 8, 0, 4, 0, 0, 1, 2, 0],
+board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
     [6, 0, 0, 0, 7, 5, 0, 0, 9],
     [0, 0, 0, 6, 0, 1, 0, 7, 8],
     [0, 0, 7, 0, 4, 0, 2, 6, 0],
@@ -21,6 +29,7 @@ board = [[7, 8, 0, 4, 0, 0, 1, 2, 0],
     [9, 0, 4, 0, 6, 0, 0, 0, 5],
     [0, 7, 0, 3, 0, 0, 0, 1, 2],
     [1, 2, 0, 0, 0, 7, 4, 0, 0],
-    [0, 4, 9, 2, 0, 6, 0, 0, 7]]
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+]
 
-print_board(board)
+# print_board(board)
