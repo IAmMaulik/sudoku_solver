7e9a3f1da564ea7c9c3f3f8061a5cb1b57b36d67
Maulik Shah <maulik.shah715@gmail.com>
2020-12-13 18:14:48 +0530

    Added a function to check whther the position is valid or not

--- solver.py
+++ solver.py
@@ -1,3 +1,4 @@
+# Printing the board out
 def print_board(bo):
     for i in range (len(bo)):
         if (i%3)==0 and i!=0:
@@ -12,6 +13,7 @@ def print_board(bo):
             else:
                 print(str(bo[i][j]) + " ", end="")
 
+
 # Finding empty squares on the sudoku board
 def find_empty(bo):
     for x in range(len(bo)):
@@ -19,7 +21,32 @@ def find_empty(bo):
             if bo[x][y] == 0:
                 return (x, y) # Coordinates of the empty square
 
-# Defing the 2d array
+
+# Is the position valid or not
+def isValid(bo, num, pos):
+    # Checking the row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1]!=i:
+            return False
+    
+    # Checking the column
+    for i in range(len(bo[0])):
+        if bo[i][pos[1]] == num and pos[0]!=i:
+            return False
+    
+    # Checking whether it is in the same box (Slightly tricky)
+    box_x = pos[1]//3
+    box_y = pos[0]//3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
+            if bo[i][j]==num and (i, j) != pos:
+                return False
+    
+    return True
+
+
+# Defining the 2d array
 board = [
     [7, 8, 0, 4, 0, 0, 1, 2, 0],
     [6, 0, 0, 0, 7, 5, 0, 0, 9],
@@ -31,5 +58,3 @@ board = [
     [1, 2, 0, 0, 0, 7, 4, 0, 0],
     [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
-
-# print_board(board)
